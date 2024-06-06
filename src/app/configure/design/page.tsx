a8ae0c59aafc0e2172297f750044ff4d76c6510d
Anas Khalifa <anasgets111@gmail.com>
2024-06-07 00:29:24 +0300

    after making most of the api calls for payment and auth

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -10,12 +10,15 @@ interface PageProps {
 
 const Page = async ({ searchParams }: PageProps) => {
   const { id } = searchParams;
+
   if (!id || typeof id !== "string") {
     return notFound();
   }
+
   const configuration = await db.configuration.findUnique({
     where: { id },
   });
+
   if (!configuration) {
     return notFound();
   }
